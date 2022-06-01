2a31a5de693d505e858e7e0adcb56ea05ffa1e92
Artem Kolyadenko <kolyadenko_a@bk.ru>
2022-06-01 11:51:04 +0700

    addressbook not started

---
+++ addressbook_tests_autoit/appmanager/HelperBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace addressbook_tests_autoit
+{
+    public class HelperBase
+    {
+        protected ApplicationManager manager;
+
+        public HelperBase(ApplicationManager manager)
+        {
+            this.manager = manager;
+        }
+    }
+}
